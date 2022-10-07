2722ee5cdc0557c26771ace9079ba70c7195a0cf
SAURABH KOCH GOHAIN <67310715+Saurabh9678@users.noreply.github.com>
2022-10-07 18:26:57 +0530

    Add files via upload

---
+++ add code here/hello_world_saurabh9678.cpp
@@ -0,0 +1,13 @@
+// LANGUAGE: C++
+// AUTHOR: Saurabh Koch Gohain
+// GITHUB: https://github.com/Saurabh9678
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello world! For Hacktober Fest 2022"<<endl;
+
+    return 0;
+}
